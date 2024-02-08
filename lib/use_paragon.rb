6e00fc46f5609ba317d3428605f1bd6ca8d5f2dc
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2024-02-08 00:46:38 -0600

    Fix rubocop alerts

--- lib/use_paragon.rb
+++ lib/use_paragon.rb
@@ -7,13 +7,15 @@ require "use_paragon/user"
 require "use_paragon/version"
 require "use_paragon/workflow"
 
+# UseParagon gem base module
 module UseParagon
+  # Configuration from initializer
   class << self
     def configuration
       @configuration ||= Configuration.new
     end
 
-    def configure(&block)
+    def configure
       yield(configuration)
     end
   end
