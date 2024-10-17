ed3d1da2d158fcf6163fd163c310522ec4c3b3ba
axcochrane <a.x.cochrane@gmail.com>
2024-10-17 14:03:50 +0100

    IMPR - add ability to set headers on Workflow.

--- lib/use_paragon/workflow.rb
+++ lib/use_paragon/workflow.rb
@@ -5,7 +5,9 @@ require "use_paragon/base"
 module UseParagon
   # API calls for workflows
   class Workflow < Base
-    def request(workflow_id, payload = {})
+    def request(workflow_id, payload: {}, headers: {})
+      # connection.headers = connection.headers.merge(headers)
+      set_headers(headers)
       connection.post(path("sdk/triggers/#{workflow_id}"), payload)
     end
 
@@ -24,6 +26,10 @@ module UseParagon
         name: event_name,
         payload: payload
       }
+    end
+
+    def set_headers(headers)
+      connection.headers = connection.headers.merge(headers)
     end
   end
 end
