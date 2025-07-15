6f40838c8dfd9fab97a5e8cfd2c5c8cabd205223
Eduardo Candanedo <eduardo.candanedo.94@gmail.com>
2025-07-15 11:30:00 -0600

    Fix specs

--- spec/use_paragon/workflow_spec.rb
+++ spec/use_paragon/workflow_spec.rb
@@ -20,7 +20,11 @@ RSpec.describe UseParagon::Workflow do
     it "calls the correct endpoint with post method" do
       expect(workflow).to receive(:path).with("sdk/events/trigger")
                                         .and_return("/projects/123/sdk/events/trigger")
-      expect(workflow).to receive(:connection).and_return(double("connection", post: true))
+      connection = double("connection")
+      allow(connection).to receive(:headers).and_return({})
+      allow(connection).to receive(:headers=)
+      allow(connection).to receive(:post).and_return(true)
+      expect(workflow).to receive(:connection).and_return(connection).exactly(3).times
       workflow.event("some_event", {})
     end
   end
@@ -29,7 +33,9 @@ RSpec.describe UseParagon::Workflow do
     it "calls the correct endpoint with delete method" do
       expect(workflow).to receive(:path).with("sdk/workflows/abc")
                                         .and_return("/projects/123/sdk/workflows/abc")
-      expect(workflow).to receive(:connection).and_return(double("connection", delete: true))
+      connection = double("connection")
+      allow(connection).to receive(:delete).and_return(true)
+      expect(workflow).to receive(:connection).and_return(connection)
       workflow.disable("abc")
     end
   end
